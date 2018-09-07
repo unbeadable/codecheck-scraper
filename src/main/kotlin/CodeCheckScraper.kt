734427886e1b0e1b3a9a3974a8442518ee98b346
Stefanie Grewenig <sgreweni@thoughtworks.com>
2018-09-08 00:29:18 +0200

    writing ean and microplasctics to file for first page of given category

--- src/main/kotlin/CodeCheckScraper.kt
+++ src/main/kotlin/CodeCheckScraper.kt
@@ -1,5 +1,6 @@
 import com.beust.klaxon.Klaxon
 import org.jsoup.Jsoup
+import java.io.File
 import java.io.InputStream
 
 
@@ -18,8 +19,20 @@ class CodeCheckScraper {
 
     fun getProductInformationForUrl(productUrl: String): Product {
         val page: ProductPage = parser.parseProductPage(Jsoup.connect(productUrl).get())
-        return Product(page.getProductName())
+        return Product(page.getEan(), page.getMicroplastic())
+    }
+
+    fun writeProductToFile(categoryUrl: String) {
+        getProductLinksForCategory(categoryUrl).forEach {
+            val page: ProductPage = parser.parseProductPage(Jsoup.connect(it).get())
+
+            if (page.hasMicroplastic() && page.getEan() != null) {
+                val text = "${page.getEan()},${page.getMicroplastic()}\n"
+                File("beadables.csv").appendText(text)
+            }
+        }
+
     }
 }
 
-class Product(val name: String)
+class Product(val ean: String?, microplastic: String?)
