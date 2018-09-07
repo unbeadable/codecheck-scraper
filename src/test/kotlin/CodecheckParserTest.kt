695a3a5af43e253e9a6c773357a57577c93c090c
Stefanie Grewenig <sgreweni@thoughtworks.com>
2018-09-07 20:51:23 +0200

    Adding some more assertions

--- src/test/kotlin/CodecheckParserTest.kt
+++ src/test/kotlin/CodecheckParserTest.kt
@@ -65,6 +65,7 @@ class CodecheckParserTest {
         val filePath: String = CodecheckParserTest::class.java.getResource("searchResults.html").file
         val page: SearchResultsPage = parser.parseSearchResultsPage(Jsoup.parse(File(filePath), "UTF-8"))
         assertThat(page.getProducts()!![0].url, `is`("https://www.codecheck.info/kosmetik_koerperpflege/gesichtspflege/gesichtsmasken/id_2096663156/Tony_Moly_Timeless_Ferment_Snail_Gel_Mask.pro"))
+        assertThat(page.getProducts()!![1].url, `is`("https://www.codecheck.info/kosmetik/gesichtspflege/gesichtscremen/ean_20714806323/id_34370165/Clinique_Pep_Start_Hydroblur_Moisturizer_Gesichtscreme_50_ml.pro"))
     }
 }
 
