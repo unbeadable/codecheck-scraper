49fc71eb336824987bb025e8ad91429708463fdb
bstein <bstein@thoughtworks.com>
2018-09-15 18:25:33 +0200

    rename runbook to logbook

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,17 +3,18 @@ import java.io.File
 import java.io.InputStream
 import java.time.LocalDateTime
 
-fun main(args: Array<String>) {
-    data class Link(val category: String, val url: String, val processed: Boolean)
-    data class Runbook(
-            var name: String = "",
-            var url: String = "",
-            var start: LocalDateTime = LocalDateTime.MIN,
-            var end: LocalDateTime = LocalDateTime.MIN,
-            var visitedLinks: Int = -1,
-            var brokenLinks: Int = -1,
-            var invalidData: Int = -1)
+data class Link(val category: String, val url: String, val processed: Boolean)
 
+data class LogBook(
+        val name: String,
+        val url: String,
+        val start: LocalDateTime,
+        val end: LocalDateTime,
+        val visitedLinks: Int,
+        val brokenLinks: Int,
+        val invalidData: Int)
+
+fun main(args: Array<String>) {
     val stream: InputStream = DataScraper::class.java.getResource("categoryLinks.json").openStream()
     val links = Klaxon().parseArray<Link>(stream) ?: throw RuntimeException("Could not parse json from file.")
     links.filterNot(Link::processed).forEach {
@@ -26,7 +27,7 @@ fun main(args: Array<String>) {
         File("results/data/cleaned-${it.category}.json").writeText(Klaxon().toJsonString(cleanedProducts))
 
         val invalidData = result.visitedLinks - result.brokenLinks - cleanedProducts.size
-        val runbook = Runbook(it.category, it.url, start, LocalDateTime.now(), result.visitedLinks, result.brokenLinks, invalidData)
-        File("results/data/runbook-${it.category}.json").writeText(Klaxon().toJsonString(runbook))
+        val logBook = LogBook(it.category, it.url, start, LocalDateTime.now(), result.visitedLinks, result.brokenLinks, invalidData)
+        File("results/data/logbook-${it.category}.json").writeText(Klaxon().toJsonString(logBook))
     }
 }
